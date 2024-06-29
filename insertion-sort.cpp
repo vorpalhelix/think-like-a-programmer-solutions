f6f43b6707ec0b90234d26f905d2931b4802be69
Ank1x <73440575+Ank1x@users.noreply.github.com>
2024-06-29 18:42:21 +0530

    additional sort problems

---
+++ insertion-sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+  const int ARRAY_SIZE = 10;
+  int intArray[ARRAY_SIZE] = {7, 28, 100, 78, 84, 98, 75, 70, 81, 68};
+
+  int start = 0;
+  int end = ARRAY_SIZE - 1;
+  int temp = 0;
+
+  for (int i = start + 1; i <= end; i++) {
+    for (int j = i; j > 0 && intArray[j - 1] > intArray[j]; j--) {
+      temp = intArray[j - 1];
+      intArray[j - 1] = intArray[j];
+      intArray[j] = temp;
+    }
+  }
+
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+  cout << intArray[i] << " ";
+}
+}
---
+++ qsort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int compareFunc(const void *, const void *);
+
+int main() {
+const int ARRAY_SIZE = 10;
+int intArray[ARRAY_SIZE] = {87, 28, 100, 78, 84, 98, 75, 70, 81, 68};
+qsort(intArray, ARRAY_SIZE, sizeof(int), compareFunc);
+
+for (int i = 0; i < ARRAY_SIZE; i++) {
+  cout << intArray[i] << " ";
+}
+}
+
+int compareFunc(const void * voidA, const void * voidB) {
+  int * intA = (int *)(voidA);
+  int * intB = (int *)(voidB);
+
+  return *intA - *intB;
+}
